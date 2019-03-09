981be325e79b8b48df5c9241f3aff0d9810b56a5
sparks42 <958575100@qq.com>
2019-03-09 15:22:33 +0800

    第一个元素不需要 margin left -1px

--- src/button-group.vue
+++ src/button-group.vue
@@ -5,10 +5,10 @@
 </template>
 <script>
   export default {
-    mounted(){
-      for(let node of this.$el.children){
+    mounted() {
+      for (let node of this.$el.children) {
         let name = node.nodeName.toLowerCase()
-        if (name!=='button'){
+        if (name !== 'button') {
           console.warn(`g-button-group 的子元素应该全是 g-button，但是你写的是${name}`)
         }
       }
@@ -16,21 +16,23 @@
   }
 </script>
 <style lang="scss">
-  .g-button-group{
+  .g-button-group {
     display: inline-flex;
     vertical-align: middle;
-    > .g-button{
+    > .g-button {
       border-radius: 0;
-      margin-left: -1px;
-      &:first-child{
+      &:not(:first-child) {
+        margin-left: -1px;
+      }
+      &:first-child {
         border-top-left-radius: var(--border-radius);
         border-bottom-left-radius: var(--border-radius);
       }
-      &:last-child{
+      &:last-child {
         border-top-right-radius: var(--border-radius);
         border-bottom-right-radius: var(--border-radius);
       }
-      &:hover{
+      &:hover {
         position: relative;z-index: 1;
       }
     }
