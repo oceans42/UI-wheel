6b77629771a5be4b95d48665209385f72de19274
sparks42 <958575100@qq.com>
2019-03-09 12:16:01 +0800

    添加 click 事件

--- src/button.vue
+++ src/button.vue
@@ -1,7 +1,8 @@
 <template>
-  <button class="g-button" :class="{[`icon-${iconPosition}`]:true}">
-    <g-icon class="icon" v-if="icon" :name="icon"></g-icon>
-    <g-icon class="loading" name="loading"></g-icon>
+  <button class="g-button" :class="{[`icon-${iconPosition}`]:true}"
+@click="$emit('click')">
+    <g-icon class="icon" v-if="icon && !loading" :name="icon"></g-icon>
+    <g-icon class="loading icon" v-if="loading" name="loading"></g-icon>
     <div class="content">
       <slot></slot>
     </div>
@@ -12,6 +13,10 @@
     //props: ['icon', 'iconPosition']
     props: {
       icon: {},
+      loading: {
+        type: Boolean,
+        default: false
+      },
       iconPosition: {
         type: String,
         default: 'left',
@@ -24,10 +29,15 @@
 </script>
 <style lang="scss">
   @keyframes spin {
-    0%{transform: rotate(0deg);}
-    100%{transform: rotate(360deg);}
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
 
   }
+
   .g-button {
     font-size: var(--font-size);
     height: var(--button-height);
@@ -62,7 +72,6 @@
     }
 
     &.icon-right {
-
       > .content {
         order: 1;
       }
@@ -73,8 +82,9 @@
         margin-left: .3em;
       }
     }
-    .loading{
-      animation:spin 2s infinite linear;
+
+    .loading {
+      animation: spin 2s infinite linear;
     }
   }
 </style>
