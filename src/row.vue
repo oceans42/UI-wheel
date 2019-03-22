ac7f3b3053fca082378379af937d7c94f26bf9b3
sparks42 <958575100@qq.com>
2019-03-22 15:31:23 +0800

    重构 row 和 col

--- src/row.vue
+++ src/row.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="row" :style="{marginLeft:-gutter/2+'px',marginRight:-gutter/2+'px'}">
+  <div class="row" :style="rowStyle">
     <slot></slot>
   </div>
 </template>
@@ -11,15 +11,24 @@
         type: [Number, String]
       }
     },
+    computed: {
+      rowStyle() {
+        let {gutter} = this
+        return {
+          marginLeft: -gutter / 2 + 'px',
+          marginRight: -gutter / 2 + 'px'
+        }
+      }
+    },
     mounted() {
-      this.$children.forEach((vm)=>{
-        vm.gutter=this.gutter
+      this.$children.forEach((vm) => {
+        vm.gutter = this.gutter
       })
     }
   }
 </script>
 <style lang="scss" scoped>
-  .row{
+  .row {
     display: flex;
   }
 </style>
